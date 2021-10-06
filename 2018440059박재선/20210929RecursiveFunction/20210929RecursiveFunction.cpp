24e83f9abba140ac0b859dc34fdfb2b2eb850ce1
moran991231 <moran991231@naver.com>
2021-10-06 14:18:16 +0900

    week5: Recursive Function, structure, linked list

---
+++ 2018440059박재선/20210929RecursiveFunction/20210929RecursiveFunction.cpp
@@ -0,0 +1,24 @@
+﻿// 20210929RecursiveFunction.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+// Recursive function (재귀함수)
+// 함수 안에서 자신을 호출
+
+void r_function(int a) {
+    r_function(a);
+}
+
+int main()
+{ 
+    int x = 10;
+    r_function(x);
+    return 11;
+} 
+
+// 디버깅하지 않고 시작-> main함수 리턴값: -1073741571
+/*
+* That is the signed integer representation of Microsoft's "stack overflow/stack exhaustion" error code 0xC00000FD.
+* STATUS_STACK_OVERFLOW
+*/
